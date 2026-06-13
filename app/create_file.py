dfcfa728603045cdef54c6dbf09ce52085e8a78e
Petro <petropavliuk12.05@gmail.com>
2026-06-13 23:01:20 +0300

    fixed -f and -d sequence

--- app/create_file.py
+++ app/create_file.py
@@ -1,46 +1,38 @@
+from datetime import datetime
 import sys
 import os
-from datetime import datetime
 
 
-command = sys.argv
-path_list = []
-d_index = None
-f_index = None
+args = sys.argv[1:]
 
-if "-d" in command:
-    d_index = command.index("-d")
-if "-f" in command:
-    f_index = command.index("-f")
+if not args:
+    raise ValueError("No arguments provided")
+
+
+if args[0] == "-d":
+    directories = args[1:]
+
+    path = os.path.join(*directories)
+
+    os.makedirs(path, exist_ok=True)
+
+
+elif args[0] == "-f":
+    filename = args[1]
+
+    with open(filename, "a") as file:
+        file.write(datetime.now().strftime("%Y-%m-%d %H:%M:%S\n"))
+
+        index = 1
+
+        while True:
+            line = input("Enter content line: ")
+
+            if line == "stop":
+                break
+
+            file.write(f"{index} {line}\n")
+            index += 1
+
 else:
-    raise Exception
-
-if d_index > f_index:
-    command.append(command.pop(f_index + 1))
-    command.insert(-1, command.pop(f_index))
-
-d_index = command.index("-d")
-f_index = command.index("-f")
-
-if d_index and f_index:
-    path_list += command[d_index + 1:f_index]
-path_list.append(command[-1])
-
-path = os.path.join(*path_list)
-directory = os.path.dirname(path)
-
-if directory:
-    os.makedirs(directory, exist_ok=True)
-
-with open(path, "a") as f:
-    index = 1
-    now = datetime.now()
-    if os.path.getsize(path) != 0:
-        f.write("\n")
-    f.writelines(now.strftime("%Y-%m-%d %H:%M:%S\n"))
-    while True:
-        line = input("Enter content line: ")
-        if line == "stop":
-            break
-        f.writelines(f"{index} {line}\n")
-        index += 1
+    raise ValueError("Unknown flag")
